1b6c611093ac456d7e05edeedc640366541ac583
ShadowCupcak <kevinespinoza923@gmail.com>
2024-06-12 20:19:51 -0600

    añadida llave foranea de eventos a la tabla de cursos

--- database/migrations/2024_06_08_234245_create_courses_table.php
+++ database/migrations/2024_06_08_234245_create_courses_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('events_id')->constrained();
             $table->string('name');
             $table->timestamps();
         });
